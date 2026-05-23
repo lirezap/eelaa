7b2fb4b4725a9fb1c5f8f039cceab495c3614ce6
Alireza Pourtaghi <alirezapt@protonmail.com>
2026-05-24 00:44:43 +0330

    Refactored.

--- src/main/java/app/eelaa/core/net/SequenceIdsHolder.java
+++ src/main/java/app/eelaa/core/net/SequenceIdsHolder.java
@@ -1,8 +1,8 @@
 package app.eelaa.core.net;
 
 /**
- * Non thread-safe but fast sequence ids holder. Designed to be used in netty's server socket channels. The purpose of
- * this class is to prevent duplicated sequence id processing, provided by a single channel (TCP connection). It holds
+ * Non thread-safe but fast sequence ids holder. Designed to be used in netty's server socket channel. The purpose of
+ * this class is to prevent duplicate sequence id processing, provided by a single channel (TCP connection). It holds
  * last N sequence ids provided and checks incoming frame's sequence id with all previously provided ones, and then
  * informs the caller about duplication.
  *
@@ -13,7 +13,7 @@ final class SequenceIdsHolder {
     private int indexToAdd;
 
     public SequenceIdsHolder() {
-        this.sequenceIds = new int[512];
+        this.sequenceIds = new int[256];
         this.indexToAdd = 0;
     }
 
@@ -23,8 +23,7 @@ final class SequenceIdsHolder {
 
         // Provided sequence id must not be duplicate in the last N provided sequence ids.
         for (int i = 1; i < sequenceIds.length; i++) {
-            final var fetchedSequenceId = sequenceIds[Math.abs(indexToAdd - i)];
-            if (fetchedSequenceId == sequenceId) return false;
+            if (sequenceIds[Math.abs(indexToAdd - i)] == sequenceId) return false;
         }
 
         // Provided sequence id must not be less than or equal to the minimum value of the last N provided sequence ids.
