3a8cd7599849bb3cc60c63a82a555981ddfab2e8
Alireza Pourtaghi <alirezapt@protonmail.com>
2026-06-27 14:51:09 +0330

    Added batch operation test.

--- src/test/java/software/openx/eelaa/storage/LMDBManagerTest.java
+++ src/test/java/software/openx/eelaa/storage/LMDBManagerTest.java
@@ -8,6 +8,7 @@ import java.nio.file.Path;
 
 import static java.lang.foreign.MemorySegment.NULL;
 import static org.junit.jupiter.api.Assertions.*;
+import static software.openx.eelaa.lmdb.LMDBFlags.MDB_NOOVERWRITE;
 
 /**
  * @author Alireza Pourtaghi
@@ -58,6 +59,32 @@ public class LMDBManagerTest {
 
             assertTrue(manager.put(dbi, key, value));
             assertEquals(NULL, manager.get(dbi, secondKey, arena));
+        }
+    }
+
+    @Test
+    public void testBatchPut() throws Exception {
+        var dataDirectoryPath = Files.createTempDirectory(String.valueOf(System.currentTimeMillis()));
+        try (var manager = LMDBManager.newInstance(Path.of(System.getenv("NATIVE_LIBRARIES_LMDB_PATH")), dataDirectoryPath, 1024 * 1024 * 1024, 1, 0, 0644);
+             var arena = Arena.ofConfined()) {
+
+            var dbi = manager.openDb("test");
+            var txn = manager.newTxn(arena, NULL, MDB_NOOVERWRITE);
+
+            for (int i = 1; i <= 1000000; i++) {
+                var key = arena.allocateFrom("key" + i);
+                var value = arena.allocateFrom("value" + i);
+
+                assertTrue(manager.put(txn, dbi, key, value));
+            }
+            manager.commitTxn(txn);
+
+            txn = manager.newTxn(arena, NULL, MDB_NOOVERWRITE);
+            for (int i = 1; i <= 1000000; i++) {
+                var key = arena.allocateFrom("key" + i);
+                assertEquals("value" + i, manager.get(txn, dbi, key, arena).getString(0));
+            }
+            manager.commitTxn(txn);
         }
     }
 }
