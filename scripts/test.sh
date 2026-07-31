aceee6790c6bb8a2e05cafb00c11f7a68bcea8f5
Alireza Pourtaghi <alirezapt@protonmail.com>
2026-08-01 00:31:16 +0330

    Reordered tests.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,5 +3,5 @@
 echo "[INFO] LIBRARIES_NATIVE_LZ4_PATH environment variable must be defined for all tests to be passed"
 echo "[INFO] LIBRARIES_NATIVE_LMDB_PATH environment variable must be defined for all tests to be passed"
 
-./mvnw clean test -DargLine="-Xmx8g" -Dtest='*,!LMDBManagerTest'
-./mvnw clean test -Dtest='LMDBManagerTest' clean
+./mvnw clean test -Dtest='LMDBManagerTest'
+./mvnw clean test -DargLine="-Xmx8g" -Dtest='*,!LMDBManagerTest' clean
